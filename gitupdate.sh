9fb2505e348fbd0b0036aac759e17c429d879a29
Bart <bartoszgancarz@gmail.com>
2017-08-24 23:40:43 +0200

    backup

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 git add .
 git status
-echo -n "Enter the Description for the Change: " [Minor Update]
-read CHANGE_MSG
-git commit -m "${CHANGE_MSG}"
+git commit -m backup
 git push origin master
 exit
